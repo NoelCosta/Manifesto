f1c97de2edd7236170996a3679a4a62d5f029de5
NoelCosta <noelcosta99@hotmail.com>
2023-02-16 00:45:54 +0100

    help

--- scripts/index.js
+++ scripts/index.js
@@ -14,22 +14,7 @@ window.addEventListener('load', function() {
       .then(data => {
           code = data;
       });
-
-    // // create a new XMLHttpRequest object
-    // var xhr = new XMLHttpRequest();
-
-    // // specify the details of the GET request
-    // xhr.open('GET', '../index.html', true);
-
-    // // set a callback function to be executed when the request's state changes
-    // xhr.onreadystatechange = function() {
-    //     // check if the request has completed successfully
-    //     if (xhr.readyState === 4 && xhr.status === 200) {
-    //         // if successful, assign the response text to a variable named "code"
-    //         code = xhr.responseText;
-    //     }
-    // };
-
+      
     /* --- Normal Mode --- */
     document.getElementById("btn_hideTerminal").addEventListener('click', function(event) {
         html.classList.remove('terminal-mode');
