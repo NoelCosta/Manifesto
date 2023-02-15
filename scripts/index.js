08f4aa326616b14ad3bb96d37dc1e9b8725d622f
NoelCosta <noelcosta99@hotmail.com>
2023-02-15 21:55:37 +0100

    test 4

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,7 @@ window.addEventListener('load', function() {
     var btn_hideTerminal = document.getElementById('btn_hideTerminal');
     var code;
 
-    fetch('../index.html', {
+    fetch('index.html', {
         cache: 'no-cache'
       })
       .then(response => response.text())
